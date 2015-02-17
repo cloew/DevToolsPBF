f891a15907c08205bfef778597bb6b270dcba605
Chris Loew <cloew123@gmail.com>
2015-02-16 23:51:54 -0700

    When creating a new command you can specify that it should private

--- pbf_dev_tools/Commands/new_command.py
+++ pbf_dev_tools/Commands/new_command.py
@@ -14,19 +14,22 @@ class NewCommand:
     def addArguments(self, parser):
         """ Add arguments to the parser """
         parser.add_argument('destination', action='store', help='The destination filename to create')
+        parser.add_argument('-p', '--private', action='store_true', help='Specify that the command is private and should not be added to the Command Map')
     
     def run(self, arguments):
         """ Run the Command """
         filepath = arguments.destination
         print "Creating PBF Command:", GetPythonClassnameFromFilename(filepath), "at:", filepath
-        self.createNewCommand(filepath)
+        self.createNewCommand(filepath, private=arguments.private)
         
-    def createNewCommand(self, filepath, template="command.py", keywords=None):
+    def createNewCommand(self, filepath, template="command.py", keywords=None, private=False):
         """ Create a new command at the filepath provided """
         if keywords is None:
             keywords = {}
         classname = GetCommandClassName(filepath)
         keywords["%CommandClassName%"] = classname
-        InsertCommandConfig().insertCommandConfig(filepath)
+        
+        if not private:
+            InsertCommandConfig().insertCommandConfig(filepath)
         template_manager.CopyTemplate(filepath, template, keywords, templates_directory=TemplatesRoot)
         
